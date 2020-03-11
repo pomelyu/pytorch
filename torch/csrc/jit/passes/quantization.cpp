9968269e4769b961a1712db62796938aacb5b727
Jerry Zhang <jerryzh168@gmail.com>
2020-03-10 19:26:58 -0700

    [quant][graphmode] quantization work for prim::If
    Summary:
    
    Test Plan:
    python test/test_jit.py
    
    Reviewers:
    .
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: d45cef7a19da737722d1aa79c40ec58c0e73375c
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34518

--- torch/csrc/jit/passes/quantization.cpp
+++ torch/csrc/jit/passes/quantization.cpp
@@ -469,7 +469,10 @@ class InsertObserversHelper {
   std::tuple<OptionalModuleVector, OptionalModuleVector, std::vector<size_t>> insertObserversFor(
       Block* block,
       script::Module& module,
-      std::unordered_set<Value*> block_observed_values,
+      // this is a reference because when we insert observer for a value
+      // in one block it is also observed in another block, we don't want to
+      // insert multiple observers for the same value
+      std::unordered_set<Value*>& block_observed_values,
       bool is_entry_point = false);
 
  private:
@@ -995,7 +998,7 @@ std::tuple<OptionalModuleVector, OptionalModuleVector, std::vector<size_t>>
 InsertObserversHelper::insertObserversFor(
       Block* block,
       script::Module& module,
-      std::unordered_set<Value*> block_observed_values,
+      std::unordered_set<Value*>& block_observed_values,
       bool is_entry_point) {
   // block input/output values, used to skip inserting observers
   // for input and output of the block, we have to insert the observers
@@ -1041,7 +1044,7 @@ InsertObserversHelper::insertObserversFor(
 
   std::stack<Block*> blocks_to_visit;
   blocks_to_visit.push(block);
-  auto* self = block->inputs()[0];
+  auto* self = block->owningGraph()->inputs()[0];
   // We first construct a map from value to the module, then
   // insert observers for them later, this is to avoid interference
   // of the inserted observers with the analysis to decide where
@@ -1083,7 +1086,7 @@ InsertObserversHelper::insertObserversFor(
         for (auto i = 0; i < n->inputs().size(); ++i) {
           if (input_observers[i] && !block_inputs_outputs.count(n->input(i))
               && !block_observed_values.count(n->input(i))
-              && observed_values_.count(n->input(i)) {
+              && observed_values_.count(n->input(i))) {
             values_to_observe[n->inputs()[i]] = *input_observers[i];
             block_observed_values.insert(n->inputs()[i]);
           }
@@ -1094,6 +1097,44 @@ InsertObserversHelper::insertObserversFor(
               && !observed_values_.count(n->output(i))) {
             values_to_observe[n->outputs()[i]] = *output_observers[i];
             block_observed_values.insert(n->outputs()[i]);
+          }
+        }
+      } else if (n->kind() == prim::If) {
+        std::vector<size_t> aggregated_observed_outputs;
+        std::vector<c10::optional<script::Module>> aggregated_output_observers;
+        for (Block* subblock : n->blocks()) {
+          // subblock has access to all the values in the scope of prim::If,
+          // so subblock_observed_values == block_observed_values
+          auto info_from_subblock = insertObserversFor(subblock, module, block_observed_values);
+          auto output_observers = std::get<1>(info_from_subblock);
+          auto subblock_observed_outputs = std::get<2>(info_from_subblock);
+          // subblock for prim::If doesn't have inputs
+          if (aggregated_observed_outputs.size() > 0) {
+            TORCH_CHECK(aggregated_observed_outputs == subblock_observed_outputs,
+                        "quantization doesn't work for the case where branches "
+                        "of `if` doesn't both return quantized/non-quantized "
+                        "values");
+          } else {
+            for (auto idx : subblock_observed_outputs) {
+              block_observed_values.insert(n->output(idx));
+            }
+            aggregated_observed_outputs = subblock_observed_outputs;
+          }
+          if (aggregated_output_observers.size() > 0) {
+            TORCH_CHECK(aggregated_output_observers == output_observers,
+                        "quantization doesn't work for the case where branches "
+                        "of `if` doesn't both return values quantized the same "
+                        "way");
+          } else {
+            for (auto i = 0; i < n->outputs().size(); ++i) {
+              if (output_observers[i] && !block_inputs_outputs.count(n->output(i))
+                  && !block_observed_values.count(n->output(i))
+                  && !observed_values_.count(n->output(i))) {
+                values_to_observe[n->output(i)] = *output_observers[i];
+                block_observed_values.insert(n->output(i));
+              }
+            }
+            aggregated_output_observers = output_observers;
           }
         }
       } else {
@@ -1108,9 +1149,9 @@ InsertObserversHelper::insertObserversFor(
             }
           }
         }
-      }
-      for (Block* subblock : n->blocks()) {
-        blocks_to_visit.push(subblock);
+        for (Block* subblock : n->blocks()) {
+          blocks_to_visit.push(subblock);
+        }
       }
     }
   }
